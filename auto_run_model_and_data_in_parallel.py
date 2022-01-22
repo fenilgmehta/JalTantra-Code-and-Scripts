fdab49ce5dda07c9f7476f4d6c54ffce4613126a
Fenil Mehta <fenilgmehta@gmail.com>
2022-01-22 09:45:11 +0530

    Update the structure and naming of Model and Data files

--- auto_run_model_and_data_in_parallel.py
+++ auto_run_model_and_data_in_parallel.py
@@ -26,16 +26,16 @@ def run_command_get_output(cmd: str) -> str:
 engine_path = "./octeract-engine-4.0.0/bin/octeract-engine"
 models_dir = "./Files/Models"
 model_to_input_mapping = {
-	"basic.mod"				: "./Files/Data/Basic",
-	"basic2.mod"			: "./Files/Data/Basic",
-	"descrete_segment.mod"	: "./Files/Data",
-	"parallel_links.mod"	: "./Files/Data",
+	"m1_basic.mod"				: "./Files/Data/m1_m2",  # q
+	"m2_basic2.mod"			: "./Files/Data/m1_m2",      # q1, q2
+	"m3_descrete_segment.mod"	: "./Files/Data/m3_m4",  # q
+	"m4_parallel_links.mod"	: "./Files/Data/m3_m4",      # q1, q2
 }
 data_files = [
-	'Sample_input_cycle_twoloop.dat',
-	'Sample_input_cycle_hanoi.dat',
-	'Sample_input_double_hanoi.dat',
-	'Sample_input_triple_hanoi.dat',
+	'd1_Sample_input_cycle_twoloop.dat',
+	'd2_Sample_input_cycle_hanoi.dat',
+	'd3_Sample_input_double_hanoi.dat',
+	'd4_Sample_input_triple_hanoi.dat',
 	# 'HG_SP_1_4.dat',
 	# 'HG_SP_2_3.dat',
 	# 'HG_SP_3_4.dat',
@@ -49,9 +49,11 @@ output_dir = "./amplandocteract_files/others"
 output_data_dir = "./amplandocteract_files/others/data"
 os.system(f'mkdir -p {output_dir}')
 os.system(f'mkdir -p {output_data_dir}')
+PID_ABOVE = 846813
 
 for model_name, data_path_prefix in model_to_input_mapping.items():
 	for ith_data_file in data_files:
+		print(run_command_get_output('tmux ls | grep "autorun_"'))
 		print(run_command_get_output('tmux ls | grep "autorun_" | wc -l'))
 		while int(run_command_get_output('tmux ls | grep "autorun_" | wc -l')) >= 3:
 			# REFER: https://stackoverflow.com/questions/34937580/get-available-memory-in-gb-using-single-bash-shell-command/34938001
@@ -63,7 +65,6 @@ for model_name, data_path_prefix in model_to_input_mapping.items():
 				# pid = int(run_command_get_output(''))
 				# # # TMUX_SERVER_PID = int(run_command_get_output("ps -e | grep 'tmux: server' | awk '{print $1}'"))  # 4573 <- manually found this
 				# # # run_command_get_output(f"pstree -aps {TMUX_SERVER_PID} | grep 'ampl,' | grep -o -E '[0-9]+' | sort -n | tail -n 3")
-				PID_ABOVE = 846813
 				for pid in run_command_get_output(r"ps -e | grep mpirun | grep -v grep | awk '{print $1}' | sort -n").split():
 					# REFER: https://bash.cyberciti.biz/guide/Sending_signal_to_Processes
 					if not (int(pid) > PID_ABOVE):
@@ -75,9 +76,12 @@ for model_name, data_path_prefix in model_to_input_mapping.items():
 				break
 			time.sleep(100)
 			# REFER: https://stackoverflow.com/a/66771847
+		short_model_name = model_name[:model_name.find('_')]
+		short_data_file_name = ith_data_file[::ith_data_file.find('_')]
+		short_uniq_combination = f'{short_model_name}_{short_data_file_name}'
 		os.system(
 			rf'''
-tmux new-session -d -s 'autorun_{model_name[:-4]}_{ith_data_file[:-4]}' './ampl.linux-intel64/ampl > "{output_dir}/{model_name[:-4]}_{ith_data_file[:-4]}.txt" 2>&1 <<EOF
+tmux new-session -d -s 'autorun_{short_uniq_combination}' './ampl.linux-intel64/ampl > "{output_dir}/{short_uniq_combination}.txt" 2>&1 <<EOF
 	model {models_dir}/{model_name}
 	data {data_path_prefix}/{ith_data_file}
 	option solver "{engine_path}";
@@ -85,10 +89,11 @@ tmux new-session -d -s 'autorun_{model_name[:-4]}_{ith_data_file[:-4]}' './ampl.
 	solve;
 	display _total_solve_time;
 	display l;
-	display {"q1,q2" if ("basic2" in model_name or "parallel_links" in model_name) else "q"};
+	display {"q1,q2" if (short_model_name in ("m2", "m4")) else "q"};
 EOF'
 			'''
 		)
 		time.sleep(2)
+		# Copy files from /tmp folder at regular intervals to avoid losing data when system deletes them automatically
 		os.system(f'cp /tmp/at*nl /tmp/at*octsol "{output_data_dir}"')
 
