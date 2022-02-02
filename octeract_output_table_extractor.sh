10f831d131846711fe11b42c6070d9640a921ab7
Fenil Mehta <fenilgmehta@gmail.com>
2022-02-02 11:17:35 +0530

    😃 Add script to extract important values from the Octeract output table

---
+++ octeract_output_table_extractor.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+echo -e "Processing file = '$1'"
+cat "$1" | cut -c50- | awk -F'  +' '
+BEGIN {f=0;}
+/(mpiexec|The best solution|^[ \t]*$|^[^ ])/ {if(f==2) f+=1;}
+{
+    if(f==2) {
+        # print;
+        if (s[$1] == 0) {
+            s[$1] = 1;
+            gsub("s", "", $3);
+            gsub("(^( |\t)+|( |\t)+$)", "", $1);
+            print $3 "," $1;
+        }
+    }
+}
+/---------------------/ {f+=1;}'
