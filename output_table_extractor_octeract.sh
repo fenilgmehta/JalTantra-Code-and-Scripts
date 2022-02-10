dd414b741595e85bfd47707abcfc23b26a77b7b4
Fenil Mehta <fenilgmehta@gmail.com>
2022-02-10 18:19:42 +0530

    😃 Maintain output row count to `8`, i.e. append lines with`,` if `rows < 8`
    - If `rows > 8`, we do nothing. However, we have to handle that case separately. It is not handled here as that situation did not occur till now.

--- output_table_extractor_octeract.sh
+++ output_table_extractor_octeract.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 echo -e "Processing file = '$1'"
 cat "$1" | cut -c50- | awk -F'  +' '
-BEGIN {f=0;}
+BEGIN {f=0; rows=0}
 /(mpiexec|The best solution|^[ \t]*$|^[^ ])/ {if(f==2) f+=1;}
 {
     if(f==2) {
@@ -12,8 +12,14 @@ BEGIN {f=0;}
             gsub("(^( |\t)+|( |\t)+$)", "", $1);
             gsub(" ", ",", $1);
             print $3 "," $1;
+            rows += 1;
         }
     }
 }
-/---------------------/ {f+=1;}'
-echo ','
+/---------------------/ {f+=1;}
+END {
+    for (; rows < 8; rows += 1) {
+        print ","
+    }
+}
+'
