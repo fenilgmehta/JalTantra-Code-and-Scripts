4360ff185ce3d980d8b0498b6d45f2df17354838
Fenil Mehta <fenilgmehta@gmail.com>
2022-06-07 12:37:12 +0530

    ♻️ Refactor code - use variables instead of hard coding paths

--- CalculateNetworkCost_JaltantraLauncher.sh
+++ CalculateNetworkCost_JaltantraLauncher.sh
@@ -11,39 +11,42 @@
 FILE_PATH=$(dirname "$0")
 cd "${FILE_PATH}"
 
-echo >> log_jaltantra_CalculateNetworkCost_JaltantraLauncher.log
-echo "date    = '$(date)'" >> log_jaltantra_CalculateNetworkCost_JaltantraLauncher.log
-echo "whoami  = '$(whoami)'" >> log_jaltantra_CalculateNetworkCost_JaltantraLauncher.log
-echo "pwd     = '$(pwd)'" >> log_jaltantra_CalculateNetworkCost_JaltantraLauncher.log
-echo "\$0      = '${0}'" >> log_jaltantra_CalculateNetworkCost_JaltantraLauncher.log
-echo "\$1      = '${1}'" >> log_jaltantra_CalculateNetworkCost_JaltantraLauncher.log
-echo "\$2      = '${2}'" >> log_jaltantra_CalculateNetworkCost_JaltantraLauncher.log
-echo "python  = '$(which python)'" >> log_jaltantra_CalculateNetworkCost_JaltantraLauncher.log
-echo "python3 = '$(which python3)'" >> log_jaltantra_CalculateNetworkCost_JaltantraLauncher.log
-dirname "${0}" >> log_jaltantra_CalculateNetworkCost_JaltantraLauncher.log
-echo "$(dirname '$0')" >> log_jaltantra_CalculateNetworkCost_JaltantraLauncher.log
-echo "${FILE_PATH}" >> log_jaltantra_CalculateNetworkCost_JaltantraLauncher.log
+LOG_FILE='log_jaltantra_CalculateNetworkCost_JaltantraLauncher.log'
+MINICONDA_HOME='/home/student/miniconda3'
+
+echo >> "${LOG_FILE}"
+echo "date    = '$(date)'" >> "${LOG_FILE}"
+echo "whoami  = '$(whoami)'" >> "${LOG_FILE}"
+echo "pwd     = '$(pwd)'" >> "${LOG_FILE}"
+echo "\$0      = '${0}'" >> "${LOG_FILE}"
+echo "\$1      = '${1}'" >> "${LOG_FILE}"
+echo "\$2      = '${2}'" >> "${LOG_FILE}"
+echo "python  = '$(which python)'" >> "${LOG_FILE}"
+echo "python3 = '$(which python3)'" >> "${LOG_FILE}"
+dirname "${0}" >> "${LOG_FILE}"
+echo "$(dirname '$0')" >> "${LOG_FILE}"
+echo "${FILE_PATH}" >> "${LOG_FILE}"
 
 if [[ -f "${1}" ]]; then
-	# >>> conda initialize >>>
-	# !! Contents within this block are managed by 'conda init' !!
-	__conda_setup="$('/home/student/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-	if [ $? -eq 0 ]; then
-	    eval "$__conda_setup"
-	else
-	    if [ -f "/home/student/miniconda3/etc/profile.d/conda.sh" ]; then
-	        . "/home/student/miniconda3/etc/profile.d/conda.sh"
-	    else
-	        export PATH="/home/student/miniconda3/bin:$PATH"
-	    fi  
-	fi
-	unset __conda_setup
-	# <<< conda initialize <<<
+    # >>> conda initialize >>>
+    # !! Contents within this block are managed by 'conda init' !!
+    __conda_setup="$(\"${MINICONDA_HOME}/bin/conda\" 'shell.bash' 'hook' 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+        eval "$__conda_setup"
+    else
+        if [ -f "${MINICONDA_HOME}/etc/profile.d/conda.sh" ]; then
+            . "${MINICONDA_HOME}/etc/profile.d/conda.sh"
+        else
+            export PATH="${MINICONDA_HOME}/bin:$PATH"
+        fi  
+    fi
+    unset __conda_setup
+    # <<< conda initialize <<<
 
-	conda activate dev
+    conda activate dev
 
-	# REFER: https://stackoverflow.com/questions/876239/how-to-redirect-and-append-both-standard-output-and-standard-error-to-a-file-wit
-	/home/student/miniconda3/envs/dev/bin/python3 CalculateNetworkCost.py -p "${1}" --solver-models 'octeract 1 2' --time "${2}" --debug > "${1}.log" 2>&1
+    # REFER: https://stackoverflow.com/questions/876239/how-to-redirect-and-append-both-standard-output-and-standard-error-to-a-file-wit
+    "${MINICONDA_HOME}/envs/dev/bin/python3" CalculateNetworkCost.py -p "${1}" --solver-models 'octeract 1 2' --time "${2}" --debug > "${1}.log" 2>&1
 else
-	echo "ERROR: either file does not exist or it is not a file: '${1}'" >> log_jaltantra_CalculateNetworkCost_JaltantraLauncher.log
+    echo "ERROR: either file does not exist or it is not a file: '${1}'" >> "${LOG_FILE}"
 fi
